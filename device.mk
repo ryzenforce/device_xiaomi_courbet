1eccacce09b96cb187c9e5141621d04be807ee97
Yo Chiang <yochiang@google.com>
2020-08-17 11:44:54 +0800

    courbet: Use on_vendor VNDK APEX
    Move VNDK APEX to /vendor partition so we don't need to update
    vendor.img prebuilt everytime there is a change in the VNDK
    libraries.
    
    Change-Id: I832191e8876306883a04c8539cf8dcda16fe940f
    Signed-off-by: klozz <klozz@thexperienceproject.org>

--- device.mk
+++ device.mk
@@ -423,6 +423,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
+# VNDK
+$(foreach target, $(shell cat $(LOCAL_PATH)/vndk.txt), $(eval PRODUCT_PACKAGES += $(target).vendor))
+
+PRODUCT_PACKAGES += \
+    com.android.vndk.current.on_vendor
+
 # Vulkan
 PRODUCT_PACKAGES += \
     libvulkan
