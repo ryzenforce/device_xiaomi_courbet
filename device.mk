3b9c736e15d630083633994806cef899ce3c0dda
AdarshGrewal <adarsh@aosip.dev>
2021-07-12 16:35:43 +0530

    courbet: Build vndservicemanager

--- device.mk
+++ device.mk
@@ -371,6 +371,7 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/xiaomi
 
+<<<<<<< HEAD
 # Telephony
 PRODUCT_PACKAGES += \
     ims-ext-common \
@@ -405,6 +406,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
+
+# Vendor service manager
+PRODUCT_PACKAGES += \
+    vndservicemanager
 
 # Vibrator
 PRODUCT_PACKAGES += \
