103654fc88b5dcd5c4a7adcefae59316c71df1f4
user2194 <nomaslimites@gmail.com>
2021-12-23 15:40:51 +0100

    courbet: initial support for ancientOS

---
+++ ancient_courbet.mk
@@ -0,0 +1,43 @@
+# Copyright (C) 2020 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit common products
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit device configurations
+$(call inherit-product, device/xiaomi/courbet/device.mk)
+
+# Inherit common Ancient configurations
+$(call inherit-product, vendor/ancient/config/common_full_phone.mk)
+
+# Inherit some common AncientOS
+ANCIENT_OFFICIAL := true
+TARGET_BOOT_ANIMATION_RES := 1080
+IS_PHONE := true
+TARGET_GAPPS_ARCH := arm64
+
+PRODUCT_NAME := ancient_ courbet
+PRODUCT_DEVICE := courbet
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Mi 11 Lite 4G
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="dipper-user 8.1.0 OPM1.171019.011 V9.5.5.0.OEAMIFA release-keys"
+
+BUILD_FINGERPRINT := Xiaomi/dipper/dipper:8.1.0/OPM1.171019.011/V9.5.5.0.OEAMIFA:user/release-keys
