334ede1fda26a6179b4662c69d7d70748a2eae04
user2194 <nomaslimites@gmail.com>
2021-12-24 22:53:39 +0100

    courbet: remove hardware/google soong namespaces

--- device.mk
+++ device.mk
@@ -286,8 +286,6 @@ PRODUCT_COPY_FILES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    hardware/google/interfaces \
-    hardware/google/pixel \
     hardware/xiaomi
 
 # Telephony
