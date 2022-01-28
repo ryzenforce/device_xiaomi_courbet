bb0c15654c559981a78b9b68efd1903a31519ef9
Aurora <skylarkaurora17@gmail.com>
2022-01-28 22:40:10 +0700

    courbet : Arcana bringup

---
+++ aosp_courbet.mk
@@ -0,0 +1,46 @@
+# Copyright (C) 2020 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit common products
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit device configurations
+$(call inherit-product, device/xiaomi/courbet/device.mk)
+
+# Inherit common ArcanaOS configurations
+$(call inherit-product, vendor/arcana/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/telephony.mk)
+$(call inherit-product, vendor/aosp/config/arcana_props.mk)
+
+# Inherit some common ArcanaOS
+TARGET_BOOT_ANIMATION_RES := 1080
+IS_PHONE := true
+ARCANA_DEVICE := Courbet
+ARCANA_MAINTAINER := AuroraTech
+WITH_GAPPS := true
+
+PRODUCT_NAME := aosp_courbet
+PRODUCT_DEVICE := courbet
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Mi 11 Lite 4G
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="walleye-user 8.1.0 OPM1.171019.011 4448085 release-keys"
+
+BUILD_FINGERPRINT := google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys
