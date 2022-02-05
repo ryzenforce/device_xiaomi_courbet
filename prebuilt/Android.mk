93f94816a16119fc748b18f2b08485a3b0436e8f
auroratech17 <auroratech17@gmail.com>
2022-02-05 17:26:35 +0700

    courbet : dont build gcamgo

--- prebuilt/Android.mk
+++ prebuilt/Android.mk
@@ -14,18 +14,6 @@
 
 LOCAL_PATH := $(call my-dir)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := GoogleCameraGo
-LOCAL_MODULE_TAGS := optional
-LOCAL_SRC_FILES := GoogleCameraGo.apk
-LOCAL_CERTIFICATE := platform
-LOCAL_MODULE_CLASS := APPS
-LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
-LOCAL_OVERRIDES_PACKAGES := Snap Camera2
-LOCAL_DEX_PREOPT := false
-LOCAL_PRODUCT_MODULE := true
-include $(BUILD_PREBUILT)
-
 # Arcanery
 include $(CLEAR_VARS)
 LOCAL_MODULE := Arcanery
