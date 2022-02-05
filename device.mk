886f5c9bdc154f9254b7939b92cf118e4ce25176
auroratech17 <auroratech17@gmail.com>
2022-02-05 17:25:02 +0700

    courbet : add some app

--- device.mk
+++ device.mk
@@ -491,3 +491,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.zram.mark_idle_delay_mins=60 \
     ro.zram.first_wb_delay_mins=1440 \
     ro.zram.periodic_wb_delay_hours=24
+
+# Arcanery
+PRODUCT_PACKAGES += \
+    Arcanery
+
+# OPWidget
+PRODUCT_PACKAGES += \
+    OpWidget
